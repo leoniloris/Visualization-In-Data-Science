2f9c20f8709eb5ccb564f1762b36f176396f530d
leoni mota loris <leoniloris1@hotmail.com>
2018-09-03 23:30:15 -1100

    Sweep the dirty under the carpet.

---
+++ vae/imports.py
@@ -0,0 +1,21 @@
+from keras.layers import *
+from keras.models import load_model
+from keras import regularizers
+from keras.models import Model
+from keras.datasets import mnist
+from keras.losses import binary_crossentropy
+from keras import backend as K
+
+import matplotlib.pyplot as plt
+
+from os import listdir
+from os.path import isfile, join
+from pathlib import Path
+import imageio
+from IPython.display import Image
+import plot
+import importlib
+
+importlib.reload(plot)
+
+import numpy as np
